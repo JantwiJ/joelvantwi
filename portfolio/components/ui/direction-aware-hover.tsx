8e2f003c4097e411ae5c6c1ae830c18e985e25e9
JantwiJ <antwi32@gmail.com>
2024-10-06 21:25:02 +0100

    added more files

--- portfolio/components/ui/direction-aware-hover.tsx
+++ portfolio/components/ui/direction-aware-hover.tsx
@@ -95,7 +95,10 @@ export const DirectionAwareHover = ({
               width="1000"
               height="1000"
               src={imageUrl}
-            />
+              style={{
+                maxWidth: "100%",
+                height: "auto"
+              }} />
           </motion.div>
           <motion.div
             variants={textVariants}
