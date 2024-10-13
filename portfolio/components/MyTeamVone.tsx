7c31ebf8651c817539b7dc64440dd64642cb10e3
JantwiJ <antwi32@gmail.com>
2024-10-13 19:14:06 +0100

    Colours Changed. to match dark mode version

--- portfolio/components/MyTeamVone.tsx
+++ portfolio/components/MyTeamVone.tsx
@@ -59,22 +59,22 @@ const Feature = ({
   return (
     <div
       className={cn(
-        "flex flex-col lg:border-r  py-10 relative group/feature dark:border-neutral-800",
+        "flex flex-col lg:border-r  py-10 relative group/feature dark:border-neutral-900",
         (index === 0 || index === 4) && "lg:border-l dark:border-neutral-800",
         index < 4 && " dark:border-neutral-800"
       )}
     >
       {index < 4 && (
-        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-r from-amber-500 to-pink-300 dark:from-indigo-500 dark:to-purple-500 pointer-events-none" />
+        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-r from-amber-500 to-pink-300 dark:from-amber-500 dark:to-pink-300 pointer-events-none" />
       )}
       {index >= 4 && (
-        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-r from-amber-500 to-pink-300 dark:from-indigo-500 dark:to-purple-500 pointer-events-none" />
+        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-r from-amber-500 to-pink-300 dark:from-amber-500 dark:to-pink-300 pointer-events-none" />
       )}
       <div className="mb-4 relative z-10 px-10 text-white dark:text-neutral-50">
         {icon}
       </div>
       <div className="text-lg font-bold mb-2 relative z-10 px-10">
-        <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 dark:bg-neutral-700 group-hover/feature:bg-blue-500 transition-all duration-200 origin-center" />
+        <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 dark:bg-amber-500 group-hover/feature:bg-pink-300 transition-all duration-200 origin-center" />
         <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-neutral-50 dark:text-neutral-700">
           {title}
         </span>
--- portfolio/components/ui/hero-highlight.tsx
+++ portfolio/components/ui/hero-highlight.tsx
@@ -29,12 +29,12 @@ export const HeroHighlight = ({
   return (
     <div
       className={cn(
-        "relative h-[40rem] flex items-center dark:bg-black justify-center w-full group",
+        "relative h-[40rem] flex items-center dark:bg-neutral-900 justify-center w-full group",
         containerClassName
       )}
       onMouseMove={handleMouseMove}
     >
-      <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800  pointer-events-none" />
+      <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-900  pointer-events-none" />
       <motion.div
         className="pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500   absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
         style={{
@@ -86,7 +86,7 @@ export const Highlight = ({
         display: "inline",
       }}
       className={cn(
-        `relative inline-block pb-1   px-1 rounded-lg bg-gradient-to-r from-amber-500 to-pink-300 dark:from-indigo-500 dark:to-purple-500`,
+        `relative inline-block pb-1   px-1 rounded-lg bg-gradient-to-r from-amber-500 to-pink-300 dark:from-amber-500 dark:to-pink-300`,
         className
       )}
     >
